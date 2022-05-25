c0a74d61f0c633bd424595a5c47e093c5b45708b
Pavel Ivanov <grammka@gmail.com>
2022-05-25 22:08:10 +0800

    update team page

--- src/views/TeamPage/TeamPage.tsx
+++ src/views/TeamPage/TeamPage.tsx
@@ -1,33 +1,30 @@
 import React from 'react'
 
-import { Text } from 'components/dataDisplay'
-
 import s from './TeamPage.module.scss'
 
 
 const team = [
-  { image: 'alex', name: 'Alex Nikolaev' },
-  { image: 'pavel', name: 'Pavel Ivanov' },
-  { image: 'egor', name: 'Egor Polyakov' },
+  { image: 'alex', name: 'Alex Nikolaev', role: 'Product', link: 'https://t.me/amijkko' },
+  { image: 'pavel', name: 'Pavel Ivanov', role: 'Frontend, UI/UX', link: 'https://t.me/pavelorso' },
+  { image: 'egor', name: 'Egor Polyakov', role: 'SmartContracts', link: 'https://t.me/poegva' },
 ]
 
 const TeamPage: React.FC = () => {
 
   return (
-    <div>
-      <Text className="mb-56 text-center" style="h1" color="gray-40">Our Team</Text>
-      <div className={s.items}>
-        {
-          team.map(({ image, name }) => (
-            <div key={name} className={s.item}>
-              <div className={s.image}>
-                <img src={`/images/${image}.jpg`} alt="" />
-              </div>
-              <div className={s.name}>{name}</div>
+    <div className={s.items}>
+      {
+        team.map(({ image, name, role, link }) => (
+          <div key={name} className={s.item}>
+            <div className={s.image}>
+              <img src={`/images/${image}.jpg`} alt="" />
             </div>
-          ))
-        }
-      </div>
+            <div className={s.name}>{name}</div>
+            <div className={s.role}>{role}</div>
+            <a className={s.link} href={link} target="_blank" rel="noreferrer">get in touch</a>
+          </div>
+        ))
+      }
     </div>
   )
 }
